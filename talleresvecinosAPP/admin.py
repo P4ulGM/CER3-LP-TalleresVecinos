c68d980990471bca79d7d12dd6168ddcf538318d
Paolo García <74685658+P4ulGM@users.noreply.github.com>
2025-07-05 19:35:06 -0400

    vistas para Anonimo y Registrado

--- talleresvecinosAPP/admin.py
+++ talleresvecinosAPP/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Juntadevecino
 
 # Register your models here.
+admin.site.register(Juntadevecino)
--- talleresvecinosAPP/models.py
+++ talleresvecinosAPP/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Juntadevecino(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name = "Junta de Vecino"
+        verbose_name_plural = "Juntas de Vecinos"
