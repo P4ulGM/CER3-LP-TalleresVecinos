39a39333c0d7d968778d64107be2e3c15c082d54
Paolo García <74685658+P4ulGM@users.noreply.github.com>
2025-07-05 22:44:06 -0400

    api: TODO

--- talleresvecinosAPP/serializers.py
+++ talleresvecinosAPP/serializers.py
@@ -11,25 +11,35 @@ class LugarSerializer(serializers.ModelSerializer):
         model = Lugar
         fields = ('id', 'nombre', 'direccion')
 
+class LugarSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = ('id', 'nombre')  
+
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = ('id', 'nombre', 'descripcion')
+
+class CategoriaSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = ('id', 'nombre')  
 
 class TallerSerializer(serializers.ModelSerializer):
     profesor = ProfesorSerializer(read_only=True)
     profesor_id = serializers.IntegerField(write_only=True)
 
 
-    lugar = LugarSerializer(read_only=True)
+    lugar = LugarSimpleSerializer(read_only=True)
     lugar_id = serializers.IntegerField(write_only=True)
 
-    categoria = CategoriaSerializer(read_only=True)
+    categoria = CategoriaSimpleSerializer(read_only=True)
     categoria_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Taller
         fields = ('id', 'titulo', 'fecha', 'duracion_horas', 'estado', 'profesor', 'profesor_id',
-                  'lugar', 'lugar_id', 'categoria', 'categoria_id', 'observacion', 'fecha_creacion', 'fecha_modificacion')
+                  'lugar', 'lugar_id', 'categoria', 'categoria_id', 'observacion')
         read_only_fields = ['fecha_creacion', 'fecha_modificacion']
 
